73505b3f29cbfa4a8d6fd702fe9342d212fbc0bc
DEM <daniel@daniel-Inspiron-5558>
2017-07-28 10:39:14 -0400

    version 0.4

--- PyGMod/__init__.py
+++ PyGMod/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "0.4"
+__author__ = "Daniel Mitchell"
 import PyGMod as PyGModCore
 PyGMod = ["Core", "application", "Objects"]
 
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyGMod',
+      version='0.4',
+      description='Graphics Module for Python',
+      author='Daniel Mitchell',
+      author_email='dmitchell@outlook.com',
+      url='https://github.com/dmitchelldm74/PyGMod/tree/master',
+      packages=['PyGMod'],
+     )
