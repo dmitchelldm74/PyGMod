e0bc167176c65daf8006b92714417355a93cee8f
dmitchelldm74 <dmitchell.dm74@gmail.com>
2017-07-30 23:19:10 -0400

    Update StyleChange.py

--- PyGMod/StyleChange.py
+++ PyGMod/StyleChange.py
@@ -1,17 +1,19 @@
 from threading import Thread
 import time
 
-def _function(target, delay, kwargs):
+def _function(target, delay, kwargs, onFinish):
     time.sleep(delay)
     for k in kwargs:
         getattr(target, k)(*kwargs[k])
+    onFinish(target)
     
 
 class StyleChange():
     def __init__(self, target=None):
         self.target = target
+        self.onFinish = lambda btn: None
         
     def wait(self, delay, **kwargs):
-        thread = Thread(target=_function, args=(self.target, delay, kwargs))
+        thread = Thread(target=_function, args=(self.target, delay, kwargs, self.onFinish))
         thread.daemon = True
         thread.start()
