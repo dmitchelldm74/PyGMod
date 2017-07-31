f83db05611eeb83bb6f8602873423d48a9a8799d
dmitchelldm74 <dmitchell.dm74@gmail.com>
2017-07-30 21:58:14 -0400

    Update AlertBox.py

--- PyGMod/AlertBox.py
+++ PyGMod/AlertBox.py
@@ -7,9 +7,9 @@ def alertBox(text, title="Alert!"):
     app.addElement("label", id="lb1", text=text, h=48)
     app.addElement("button", id="btn1", text="Ok", radius=0.3, onClick=lambda btn: app.exit())
     app.loop()
-    
-def startAlert(text):
-    thread = Thread(target=os.system, args=('python -c "import PyGMod;PyGMod.alertBox(\'%s\')"'%(text), ))
+ 
+def startAlert(text, **kwargs):
+    thread = Thread(target=os.system, args=('%s -c "import sys;assert sys.version[0]==\'2\';import PyGMod;PyGMod.alertBox(\'%s\')"'%(kwargs.get("PYTHON_PREFIX", "python"), text), ))
     thread.daemon = True
     thread.start()
     
